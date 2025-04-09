7f7c0dd078aa5769bfddee599173925b736dcaca
Luka Ramon Tinarelli <lukaramontinarelli47@gmail.com>
2025-04-09 17:56:04 +0200

    Update tablero.py

--- tablero.py
+++ tablero.py
@@ -1,11 +1,6 @@
 
 tablero = [["-" for _ in range(9)] for _ in range(9)]
 
-for i, letra in enumerate("ABCDEFGH"):
-    tablero[i][8] = str(8 - i)
-    tablero[8][i] = letra  
-
-
 print(tablero)
 
 for fila in tablero:
